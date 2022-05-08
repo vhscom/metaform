5244b39ee8e0023534311c65d7fd924cb9346b29
vhs <vhsdev@tutanota.com>
2022-05-09 01:09:36 +0800

    ci: 👷 support cloudflare functions

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -28,17 +28,53 @@ interface ExportPayload {
 	data: LogseqExport;
 }
 
+function scrape(text: string): Array<string | null> {
+	const re =
+		/Content-Disposition: form-data; name="file"; filename="(.*)"\r\nContent-Type:\s(.*)\r\n/;
+	const match = re.exec(text);
+
+	if (match?.length && match[1] && match[2]) {
+		return [match[1], match[2]];
+	}
+
+	return [null, null];
+}
+
 export const post: RequestHandler = async ({ request }) => {
+	const text = await request.clone().text();
 	const formData = await request.formData();
 	const file = formData.get('file') as File;
 
-	const payload: ExportPayload = {
-		name: file.name,
-		type: file.type,
-		size: file.size,
-		last_modified: file.lastModified,
-		data: JSON.parse(await file.text())
-	};
+	let payload: ExportPayload;
+
+	/**
+	 * Cloudflare returns string for File unless a flag is set. This will change
+	 * in the future though it will likely require enabling a compatibility. Until
+	 * then scrape the filename and grab the mime type from the request text.
+	 *
+	 * Ref: https://developers.cloudflare.com/workers/platform/compatibility-dates/#formdata-parsing-supports-file
+	 * Ref: https://community.cloudflare.com/t/cannot-seem-to-send-multipart-form-data/163491/3
+	 *
+	 * // return { status: 418, body: { message: 'Short at stout' } };
+	 */
+	if (typeof file === 'string') {
+		const [name, type] = scrape(text);
+		payload = {
+			name: name ?? 'Unknown',
+			type: type ?? 'Unknown',
+			size: new Blob([file]).size,
+			last_modified: new Date().getTime(),
+			data: JSON.parse(file as unknown as string)
+		};
+	} else {
+		payload = {
+			name: file.name,
+			type: file.type,
+			size: file.size,
+			last_modified: file.lastModified,
+			data: JSON.parse(await file.text())
+		};
+	}
 
 	const { data: body, error } = await supabase.from('export').insert(payload);
 

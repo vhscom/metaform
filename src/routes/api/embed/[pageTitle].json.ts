cc0c4892ed784c50efb4a6c6ca844484d0f877b5
vhs <vhsdev@tutanota.com>
2022-05-14 16:11:05 +0800

    feat: ✨ display page embed names

---
+++ src/routes/api/embed/[pageTitle].json.ts
@@ -0,0 +1,24 @@
+import { supabase } from '$lib/core/services';
+import type { RequestHandler } from '@sveltejs/kit';
+import { containsEncodedComponents, embedsForPage } from '$lib/utils/endpoint';
+
+export const get: RequestHandler = async ({ params }) => {
+	const title = containsEncodedComponents(params.title)
+		? decodeURIComponent(params.title)
+		: params.title;
+
+	const { data: page, error } = await supabase
+		.from('page')
+		.select('*')
+		.eq('page-name', title)
+		.single();
+
+	if (error) {
+		return { status: 404, error: 'Error fetching page reported by server.' };
+	}
+
+	return {
+		// headers: { 'content-type': 'application/json' },
+		body: { page, embeds: embedsForPage(page) }
+	};
+};
